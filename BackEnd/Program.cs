94253afed90f5876669944697d25f6e51557bfbb
Guilder Milliner <guilderw@gmail.com>
2023-01-03 14:08:16 -0600

    MVP for frontend

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -16,11 +16,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://192.168.72.130:4200"));
 
 app.UseHttpsRedirection();
 
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  users:any;
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://192.168.72.130:5000/api/Users')
+      .subscribe({
+        next:response => this.users = response,
+          error: error => console.log(error),
+      complete:() => console.log('Request has completed')})
+  }
 }
