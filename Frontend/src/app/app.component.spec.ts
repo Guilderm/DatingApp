b6e37f3af8c5c6afecfd5b65d1d5ae71d183442e
Guilder Milliner <guilderw@gmail.com>
2023-01-03 02:16:29 -0600

    Renamed file from Client to Front end

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'Client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Client');
+    expect(app.title).toEqual('Frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Frontend app is running!');
   });
 });
