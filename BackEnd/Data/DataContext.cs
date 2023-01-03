311a53388895c8aa54637ab130965fdf14688346
Guilder Milliner <guilderw@gmail.com>
2023-01-02 23:45:36 -0600

    Added entity Framework

---
+++ BackEnd/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using BackEnd.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Data;
+
+public class DataContext:DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+ 
+    }
+    public  DbSet<AppUser> Users { get; set; }
+}
