f59cae52f52308341833feeabb3705b820eebacb
Guilder W. Milliner <guilderw@gmail.com>
2023-01-04 17:22:19 -0600

    End of lesson 37

--- BackEnd/Controllers/AccountController.cs
+++ BackEnd/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using BackEnd.Data;
+using BackEnd.DTO;
 using BackEnd.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd.Controllers;
 
@@ -16,18 +18,28 @@ public class AccountController : BaseController
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string username, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username))
+        {
+            return BadRequest($"A user whit the name \" {registerDto.Username} \" already exist");
+        }
+
         using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
         _dataContext.Users.Add(user);
         await _dataContext.SaveChangesAsync();
         return user;
+    }
+
+    private async Task<bool> UserExists(string userName)
+    {
+        return await _dataContext.Users.AnyAsync(x => x.UserName == userName.ToLower());
     }
 }
---
+++ BackEnd/DTO/RegisterDTO.cs
@@ -0,0 +1,7 @@
+namespace BackEnd.DTO;
+
+public class RegisterDto
+{
+   public string Username { get; set; }
+   public string Password { get; set; }
+}
