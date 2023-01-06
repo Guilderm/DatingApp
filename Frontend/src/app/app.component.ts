0d6a629d864bd800003f0f2dda81736218f0bc8d
Guilder W. Milliner <guilderw@gmail.com>
2023-01-06 17:04:31 -0600

    End of section 5 - All working

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {User} from './_models/user';
 import {AccountService} from './_services/account.service';
 
@@ -10,24 +9,11 @@ import {AccountService} from './_services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users: any;
-
-  constructor(private http: HttpClient, private accountService: AccountService) {
-  }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser()
   }
-
-  getUsers() {
-    this.http.get('http://192.168.2.170:5000/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
-
   setCurrentUser() {
     const userString = localStorage.getItem('user');
     if (!userString) return;
