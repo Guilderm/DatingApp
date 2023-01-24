91eaacb3257464e6e831238af6c614ad037cfaa9
Guilder W. Milliner <guilderw@gmail.com>
2023-01-23 19:30:17 -0600

    Lesson 163 - fully working

--- BackEnd/Controllers/AccountController.cs
+++ BackEnd/Controllers/AccountController.cs
@@ -8,75 +8,76 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace BackEnd.Controllers;
-
-public class AccountController : BaseApiController
+namespace BackEnd.Controllers
 {
-    private readonly DataContext _context;
-    private readonly ITokenService _tokenService;
-    private readonly IMapper _mapper;
-
-    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+    public class AccountController : BaseApiController
     {
-        _mapper = mapper;
-        _tokenService = tokenService;
-        _context = context;
-    }
-
-    [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
-    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-    {
-        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-
-        var user = _mapper.Map<AppUser>(registerDto);
-
-        using var hmac = new HMACSHA512();
-
-        user.UserName = registerDto.Username.ToLower();
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-        user.PasswordSalt = hmac.Key;
-
-        _context.Users.Add(user);
-        await _context.SaveChangesAsync();
-
-        return new UserDto
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+        private readonly IMapper _mapper;
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
-            Username = user.UserName,
-            Token = _tokenService.CreateToken(user),
-            KnownAs = user.KnownAs,
-            Gender = user.Gender
-        };
-    }
+            _mapper = mapper;
+            _tokenService = tokenService;
+            _context = context;
+        }
 
-    [HttpPost("login")]
-    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
-    {
-        var user = await _context.Users
-            .Include(p => p.Photos)
-            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
-
-        if (user == null) return Unauthorized("invalid username");
-
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (var i = 0; i < computedHash.Length; i++)
-            if (computedHash[i] != user.PasswordHash[i])
-                return Unauthorized("invalid password");
-
-        return new UserDto
+        [HttpPost("register")] // POST: api/account/register?username=dave&password=pwd
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            Username = user.UserName,
-            Token = _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-            KnownAs = user.KnownAs,
-            Gender = user.Gender
-        };
-    }
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-    private async Task<bool> UserExists(string username)
-    {
-        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            var user = _mapper.Map<AppUser>(registerDto);
+
+            using var hmac = new HMACSHA512();
+
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if (user == null) return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+            }
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
     }
 }
--- BackEnd/Helpers/LogUserActivity.cs
+++ BackEnd/Helpers/LogUserActivity.cs
@@ -2,21 +2,22 @@ using BackEnd.Extensions;
 using BackEnd.Interfaces;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace BackEnd.Helpers;
-
-public class LogUserActivity : IAsyncActionFilter
+namespace BackEnd.Helpers
 {
-    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    public class LogUserActivity : IAsyncActionFilter
     {
-        var resultContext = await next();
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
 
-        if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
+            if (!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-        var userId = resultContext.HttpContext.User.GetUserId();
+            var userId = resultContext.HttpContext.User.GetUserId();
 
-        var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-        var user = await repo.GetUserByIdAsync(int.Parse(userId));
-        user.LastActive = DateTime.UtcNow;
-        await repo.SaveAllAsync();
+            var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
+            var user = await repo.GetUserByIdAsync(int.Parse(userId));
+            user.LastActive  = DateTime.UtcNow;
+            await repo.SaveAllAsync();
+        }
     }
 }
