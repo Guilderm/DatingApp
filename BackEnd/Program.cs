ecb405b23a588f2c7bf41257256e5a0eb7a4c0f5
Guilder W. Milliner <guilderw@gmail.com>
2023-01-29 13:24:45 -0600

    getting ready to poblish

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -3,11 +3,10 @@ using API.Entities;
 using API.Extensions;
 using API.Middleware;
 using API.SignalR;
-
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -15,44 +14,42 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
-string connString = "";
+var connString = "";
 if (builder.Environment.IsDevelopment())
-{
-	connString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
 else
 {
-	// Use connection string provided at runtime by FlyIO.
-	string connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    // Use connection string provided at runtime by FlyIO.
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-	// Parse connection URL to connection string for Npgsql
-	connUrl = connUrl.Replace("postgres://", string.Empty);
-	string pgUserPass = connUrl.Split("@")[0];
-	string pgHostPortDb = connUrl.Split("@")[1];
-	string pgHostPort = pgHostPortDb.Split("/")[0];
-	string pgDb = pgHostPortDb.Split("/")[1];
-	string pgUser = pgUserPass.Split(":")[0];
-	string pgPass = pgUserPass.Split(":")[1];
-	string pgHost = pgHostPort.Split(":")[0];
-	string pgPort = pgHostPort.Split(":")[1];
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
 
-	connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+    connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
 builder.Services.AddDbContext<DataContext>(opt =>
 {
-	opt.UseNpgsql(connString);
+    opt.UseNpgsql(connString);
 });
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder
-	.AllowAnyHeader()
-	.AllowAnyMethod()
-	.AllowCredentials()
-	.WithOrigins("https://localhost:4200"));
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -65,21 +62,21 @@ app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
 app.MapFallbackToController("Index", "Fallback");
 
-using IServiceScope scope = app.Services.CreateScope();
-IServiceProvider services = scope.ServiceProvider;
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
 try
 {
-	DataContext context = services.GetRequiredService<DataContext>();
-	UserManager<AppUser> userManager = services.GetRequiredService<UserManager<AppUser>>();
-	RoleManager<AppRole> roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-	await context.Database.MigrateAsync();
-	await Seed.ClearConnections(context);
-	await Seed.SeedUsers(userManager, roleManager);
+    var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+    await context.Database.MigrateAsync();
+    await Seed.ClearConnections(context);
+    await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
 {
-	ILogger<Program> logger = services.GetService<ILogger<Program>>();
-	logger.LogError(ex, "An error occurred during migration");
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
 }
 
 app.Run();
